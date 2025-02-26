e25be62c6870d16bf12e87e81e7094199ab1a363
Sluip430 <82076886+Sluip430@users.noreply.github.com>
2025-02-26 23:51:14 +0200

    finish second task

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CountryController } from './country.controller';
 import { CountryService } from './country.service';
 import { DateNagerModule } from 'src/services/date-nager/date-nager.module';
+import { CountriesnowModule } from 'src/services/countriesnow/countriesnow.module';
 
 @Module({
     controllers: [CountryController],
     providers: [CountryService],
-    imports: [DateNagerModule],
+    imports: [DateNagerModule, CountriesnowModule],
 })
 export class CountryModule {}
